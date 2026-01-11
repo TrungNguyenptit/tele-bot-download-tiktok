53999e5e3320569d01e73e0aca882d177f436ca8
TrungNguyenptit <trungnguyenptit2204@gmail.com>
2026-01-11 21:41:01 +0700

    df

--- bot.py
+++ bot.py
@@ -8,8 +8,6 @@ from telegram.ext import (
     filters,
 )
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-print("BOT_TOKEN =", repr(BOT_TOKEN))
 
 DOWNLOAD_DIR = "downloads"
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
@@ -45,6 +43,8 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(f"Error: {e}")
 
 def main():
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    print("BOT_TOKEN =", repr(BOT_TOKEN))
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.run_polling()
